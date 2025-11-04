71e6edd5ad2f888778576d31e7e75a5f4ecd638a
Jeifer Leon <jeiferleon@gmail.com>
2025-11-04 16:24:23 -0500

    feat: Implement player movement and rotation mechanics; update scene and tag settings

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float speed = 5.0f;
+    private Rigidbody2D rb;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // Movimiento
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+
+        if (movement.magnitude > 0.1f)
+        {
+            rb.velocity = movement.normalized * speed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+
+        // Rotación hacia el mouse
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePosition - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
