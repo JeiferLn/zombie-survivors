df280d8b4fb883426eefee23190538e7c521c8e8
Mark Edward <markbarrera@vakano.studio>
2025-11-10 15:23:18 -0500

    feat: add OnEnemyActivated event to EnemyActivationZone for improved enemy activation handling

--- Assets/Scripts/Enemies/EnemyActivationZone.cs
+++ Assets/Scripts/Enemies/EnemyActivationZone.cs
@@ -1,15 +1,22 @@
+using System;
 using UnityEngine;
 
 public class EnemyActivationZone : MonoBehaviour
 {
+    
+    public Action OnEnemyActivated = delegate { };
+    
+    
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Enemy") && other.TryGetComponent(out EnemyActivation enemy))
         {
+            OnEnemyActivated?.Invoke();
             enemy.SetVisible(true);
         }
     }
 
+    
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Enemy") && other.TryGetComponent(out EnemyActivation enemy))
