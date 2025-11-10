b660476bd8afec155e072b3f11e36ad2abbad755
Mark Edward <markbarrera@vakano.studio>
2025-11-10 15:23:05 -0500

    feat: enhance EnemyManager to manage active enemies and update their positions towards targets

--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -1,16 +1,62 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+   [Header("Pool Enemy Reference")]
+   [SerializeField] private Transform poolEnemyContainer;
+   
+   [Header("ActivationZone Reference")]
+   [SerializeField] private EnemyActivationZone activationZone;
+   
+   [Header("Targets")]
+   [SerializeField] private Transform mainTarget;
+   [SerializeField] private Transform secondaryTarget;
+   
+   // list of active enemies
+   private List<Enemy> activeEnemies = new List<Enemy>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+   private void OnEnable()
+   {
+      activationZone.OnEnemyActivated += GetActiveEnemies;
+   }
+   
+   
+   private void OnDisable()
+   {
+      activationZone.OnEnemyActivated -= GetActiveEnemies;
+   }
+
+   
+   private void Start()
+   {
+      GetActiveEnemies();
+   }
+
+   
+   private void GetActiveEnemies()
+   {
+      activeEnemies = new List<Enemy>();
+      foreach (Transform child in poolEnemyContainer)
+      {
+         if (child.TryGetComponent(out Enemy enemy))
+         {
+            activeEnemies.Add(enemy);
+         }
+      }
+   }
+   
+   
+   private void Update()
+   {
+      if (activeEnemies.Count > 0 )
+      {
+         foreach (Enemy enemy in activeEnemies)
+         {
+            enemy.transform.position = 
+               Vector2.MoveTowards(enemy.transform.position, mainTarget.position, 5f * Time.deltaTime);
+         }
+      }
+   }
 }
