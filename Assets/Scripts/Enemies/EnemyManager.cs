30efc87a5771fdfd6d2694e30243dce180aa5077
Mark-Barrera <haseo001@gmail.com>
2025-11-10 21:54:06 -0500

    feat: update EnemyManager to move only active enemies in Moving state

--- Assets/Scripts/Enemies/EnemyManager.cs
+++ Assets/Scripts/Enemies/EnemyManager.cs
@@ -3,60 +3,60 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-   [Header("Pool Enemy Reference")]
-   [SerializeField] private Transform poolEnemyContainer;
-   
-   [Header("ActivationZone Reference")]
-   [SerializeField] private EnemyActivationZone activationZone;
-   
-   [Header("Targets")]
-   [SerializeField] private Transform mainTarget;
-   [SerializeField] private Transform secondaryTarget;
-   
-   // list of active enemies
-   private List<Enemy> activeEnemies = new List<Enemy>();
+    [Header("Pool Enemy Reference")] [SerializeField]
+    private Transform poolEnemyContainer;
+
+    [Header("ActivationZone Reference")] [SerializeField]
+    private EnemyActivationZone activationZone;
+
+    [Header("Targets")] [SerializeField] private Transform mainTarget;
+    [SerializeField] private Transform secondaryTarget;
+
+    // list of active enemies
+    private List<Enemy> activeEnemies = new List<Enemy>();
 
 
-   private void OnEnable()
-   {
-      activationZone.OnEnemyActivated += GetActiveEnemies;
-   }
-   
-   
-   private void OnDisable()
-   {
-      activationZone.OnEnemyActivated -= GetActiveEnemies;
-   }
+    private void OnEnable()
+    {
+        activationZone.OnEnemyActivated += GetActiveEnemies;
+    }
 
-   
-   private void Start()
-   {
-      GetActiveEnemies();
-   }
 
-   
-   private void GetActiveEnemies()
-   {
-      activeEnemies = new List<Enemy>();
-      foreach (Transform child in poolEnemyContainer)
-      {
-         if (child.TryGetComponent(out Enemy enemy))
-         {
-            activeEnemies.Add(enemy);
-         }
-      }
-   }
-   
-   
-   private void Update()
-   {
-      if (activeEnemies.Count > 0 )
-      {
-         foreach (Enemy enemy in activeEnemies)
-         {
-            enemy.transform.position = 
-               Vector2.MoveTowards(enemy.transform.position, mainTarget.position, 5f * Time.deltaTime);
-         }
-      }
-   }
+    private void OnDisable()
+    {
+        activationZone.OnEnemyActivated -= GetActiveEnemies;
+    }
+
+
+    private void Start()
+    {
+        GetActiveEnemies();
+    }
+
+
+    private void GetActiveEnemies()
+    {
+        activeEnemies = new List<Enemy>();
+        foreach (Transform child in poolEnemyContainer)
+        {
+            if (child.TryGetComponent(out Enemy enemy))
+            {
+                activeEnemies.Add(enemy);
+            }
+        }
+    }
+
+
+    private void Update()
+    {
+        if (activeEnemies.Count > 0)
+        {
+            foreach (Enemy enemy in activeEnemies)
+            {
+                if (enemy.enemyCurrentState == EnemyState.Moving)
+                    enemy.transform.position =
+                        Vector2.MoveTowards(enemy.transform.position, mainTarget.position, 5f * Time.deltaTime);
+            }
+        }
+    }
 }
