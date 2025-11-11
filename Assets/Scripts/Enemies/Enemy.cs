1c2ed092d6cab8c1fa9a43e16cd167a8eb96c457
Mark-Barrera <haseo001@gmail.com>
2025-11-10 21:53:15 -0500

    feat: enhance Enemy class with state management and health system

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,31 +1,55 @@
-using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour, IDamageable
 {
-    // Components to disable/enable
-    private SpriteRenderer spr;
-    private BoxCollider2D boxColl2D;
-    private IDamageable _damageableImplementation;
-    
-    // Enemy active state (active/inactive)
-    public bool isActive { get; private set; }
-    
-    private void Awake()
-    {
-        spr = GetComponent<SpriteRenderer>();
-        boxColl2D = GetComponent<BoxCollider2D>();
-    }
+    // Enemy states behavior
+    public EnemyState enemyCurrentState;
 
-    public void SetVisible(bool isVisible)
+    // Enemy Template
+    public EnemyType enemyType;
+
+    // Enemy Stats
+    private float health = 0;
+    private float moveSpeed = 0;
+    private float expAmount = 0;
+
+
+    private void Start()
     {
-        isActive = isVisible;
-        if(spr!=null) spr.enabled = isVisible;
-        if(boxColl2D!=null) boxColl2D.enabled = isVisible;
+        // always start in idle state
+        enemyCurrentState = EnemyState.Idle;
+        SetEnemyStats();
+    }
+    
+    private void SetEnemyStats()
+    {
+        health = enemyType.maxHealth;
+        moveSpeed = enemyType.moveSpeed;
+        expAmount = enemyType.expAmount;
+    }
+    
+
+    public void SetInZoneState()
+    {
+        if (enemyCurrentState == EnemyState.Moving) return;
+        
+        int initialState = Random.Range(0, 2);
+        enemyCurrentState = initialState == 0 ? EnemyState.Idle : EnemyState.Moving;
     }
 
     public void TakeDamage(float amount)
     {
-      Debug.Log($"aww shiet! {amount} Damage");
+        Debug.Log($"aww shiet! {amount} Damage");
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        gameObject.SetActive(false);
     }
 }
