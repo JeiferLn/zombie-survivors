63768077c9f4fe3f2cb4b5fcab2fe09f781f9a42
Mark-Barrera <haseo001@gmail.com>
2025-11-10 21:53:37 -0500

    feat: integrate Enemy state management into visibility control

--- Assets/Scripts/Enemies/EnemyActivation.cs
+++ Assets/Scripts/Enemies/EnemyActivation.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
-using System.Collections;
 
 public class EnemyActivation : MonoBehaviour
 {
     // components to enable/disable
     private SpriteRenderer spr;
     private BoxCollider2D boxColl2D;
-   
-    
+    private Enemy enemy;
+
+
     private void Awake()
     {
         spr = GetComponent<SpriteRenderer>();
         boxColl2D = GetComponent<BoxCollider2D>();
+        enemy = GetComponent<Enemy>();
     }
 
     public void SetVisible(bool isVisible)
     {
-        if(spr!=null) spr.enabled = isVisible;
-        if(boxColl2D!=null) boxColl2D.enabled = isVisible;
+        if (spr != null) spr.enabled = isVisible;
+        if (boxColl2D != null) boxColl2D.enabled = isVisible;
+        enemy.SetInZoneState();
     }
-    
 }
